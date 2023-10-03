96560c59e3a4bbf3e93bc175c958e42be73c4fae
getuengda <batworg12@gmail.com>
2023-10-02 23:36:59 -0400

    minor fix in while loop class activity

--- Loop_ClassActivity/src/subtractnumberwhile/SubtractNumberWhile.java
+++ Loop_ClassActivity/src/subtractnumberwhile/SubtractNumberWhile.java
@@ -3,13 +3,13 @@ package subtractnumberwhile;
 public class SubtractNumberWhile {
 	// write a while loop ... while a number is greater than 0 subtract 1 from the
 	// number starting at 10.
-	public SubtractNumberWhile() {
-		int number = 0;
+	public static void main(String[] args) {
+		int number = 10;
 		while(number > 0) {
-			int i = 0;
-			
-			
+			number--;
+			System.out.println(number);		
 		}
+		
 	}
 
 }
